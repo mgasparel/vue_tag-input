5ff9d53e8f6d846d7f8fb29cf40705471ce3b41b
Mike Gasparelli <magasparel@gmail.com>
2018-01-12 00:05:32 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,46 @@
+Vue.component('tag-input', {
+    template: `
+        <div>
+            <span v-for="tag in tags">
+                {{ tag }}
+                <span @click="del(tag)">x</span>
+            </span>
+            <input type="text" ref="test" v-model="input" @keydown.tab.prevent="add" @keydown.enter.prevent="add">
+        </div>
+    `,
+
+    data() {
+        return {
+            input: '',
+            tags: []
+        }
+    },
+
+    methods: {
+        add() {
+            let text = this.input.trim();
+
+            if ( this.tags.indexOf(text) === -1 ){
+                this.tags.push(text);
+            }
+
+            this.input = '';
+
+            this.$refs.test.focus();
+        },
+
+        del(tag) {
+            let index = this.tags.indexOf(tag)
+
+            if ( index !== -1 ){
+                this.tags.splice(index, 1);
+            }
+
+            this.$refs.test.focus();
+        }
+    }
+});
+
+new Vue({
+    el: '#app'
+})
